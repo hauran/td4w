f1dafeffe2f8eff10dfb7aa4c5ee2a8e8a297770
Richard Mai <richardmai@gmail.com>
2014-05-09 17:40:55 -0400

    dont overwrite existing pre-commit file;

--- bin/td4w.js
+++ bin/td4w.js
@@ -1,7 +1,25 @@
 #!/usr/bin/env node
 var fs = require('fs');
+var colors = require('colors');
 
 (function() {
-  fs.createReadStream('pre-commit').pipe(fs.createWriteStream('../../.git/hooks/pre-commit'));
-  fs.chmodSync('../../.git/hooks/pre-commit', '777');
+  var good = true;
+  if (fs.existsSync('../../.git/hooks/pre-commit')) {  //if already a pre-commit
+    var precommit = fs.readFileSync('../../.git/hooks/pre-commit','utf8');
+    if(precommit.indexOf('TD4W')==-1) { // if precommit isnt TD4W then continue. otherwise will just overwrite
+      if(!fs.existsSync('../../.git/hooks/pre-commit_bak')) { //otherwise check if there is already a pre-commit_bak
+       fs.renameSync('../../.git/hooks/pre-commit', '../../.git/hooks/pre-commit_bak')  //if not, rename current pre-commit to pre-commit_bak
+       fs.chmodSync('../../.git/hooks/pre-commit_bak', '777');
+      }
+      else {  // crap already a pre-commit_bak. dont do anything
+        console.log('TD4W FAIL! ./git/hook/pre-commit_bak file already exists'.red) 
+        good = false;
+      }
+    }
+  }
+  if(good) {
+    fs.createReadStream('pre-commit').pipe(fs.createWriteStream('../../.git/hooks/pre-commit'));
+    fs.chmodSync('../../.git/hooks/pre-commit', '777');
+  }
+
 }).call(this)
